f812308fbd6400aa729a8b48ab7fab510fef086e
Whytebread <196115419+Whytebread@users.noreply.github.com>
2025-10-15 11:16:39 -0500

    added global variables mapped to the input, operator, and number buttons

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 // global variables
+const calculatorDisplay = document.querySelector("#numberInput");
+
+// operator buttons
+const addButton = document.querySelector("#addButton");
+const subtractButton = document.querySelector("#subtractButton");
+const multiplyButton = document.querySelector("#multiplyButton");
+const divideButton = document.querySelector("#divideButton");
+const equalsButton = document.querySelector("#equalsButton");
+const clearButton = document.querySelector("#clearButton");
+
+// number buttons
+const oneButton = document.querySelector("#one");
+const twoButton = document.querySelector("#two");
+const threeButton = document.querySelector("#three");
+const fourButton = document.querySelector("#four");
+const fiveButton = document.querySelector("#five");
+const sixButton = document.querySelector("#six");
+const sevenButton = document.querySelector("#seven");
+const eightButton = document.querySelector("#eight");
+const nineButton = document.querySelector("#nine");
+const zeroButton = document.querySelector("#zero");
+
 
 // calculator functions
 function addNumbers(){
