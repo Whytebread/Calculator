fcf208b09805a747cb9cfc83a682ed2b88d50d1a
Whytebread <196115419+Whytebread@users.noreply.github.com>
2025-10-21 14:46:38 -0500

    repalced multiple global variables for number buttons with a queryselectorall

--- main.js
+++ main.js
@@ -10,16 +10,10 @@ const equalsButton = document.querySelector("#equalsButton");
 const clearButton = document.querySelector("#clearButton");
 
 // number buttons
-const oneButton = document.querySelector("#one");
-const twoButton = document.querySelector("#two");
-const threeButton = document.querySelector("#three");
-const fourButton = document.querySelector("#four");
-const fiveButton = document.querySelector("#five");
-const sixButton = document.querySelector("#six");
-const sevenButton = document.querySelector("#seven");
-const eightButton = document.querySelector("#eight");
-const nineButton = document.querySelector("#nine");
-const zeroButton = document.querySelector("#zero");
+const numberButtons = document.querySelectorAll(".number");
+
+// event listener for number buttons
+
 
 
 // calculator functions
